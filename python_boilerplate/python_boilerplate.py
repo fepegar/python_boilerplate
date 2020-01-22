bae0f8841113cd62bad399fc5a8864ad4aa3d39b
Fernando Perez-Garcia <fepegar@gmail.com>
2020-01-22 00:39:46 +0000

    Update config

--- python_boilerplate/python_boilerplate.py
+++ python_boilerplate/python_boilerplate.py
@@ -1,1 +1,8 @@
 """Main module."""
+
+class Car:
+    """Car docstring"""
+    def __init__(self, a, b=None):
+        """Car init docstring"""
+        self.a = a
+        self.b = b
--- setup.py
+++ setup.py
@@ -19,17 +19,15 @@ test_requirements = [ ]
 setup(
     author="Audrey Roy Greenfeld",
     author_email='audreyr@example.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
     description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
     entry_points={
